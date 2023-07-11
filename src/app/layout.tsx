7998ac927bf992f98dadaf46a992101567936f5f
EseAlli <essealli.ea@gmail.com>
2023-07-11 16:18:54 +0100

    clean up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import "./globals.css";
 import "react-perfect-scrollbar/dist/css/styles.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
 
