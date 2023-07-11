a9116fa24849f19bed9cd4cf0ea37adf1aa75ed8
EseAlli <essealli.ea@gmail.com>
2023-07-11 16:02:18 +0100

    clean up and added correct metadata title and description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,9 @@
-"use client";
-import Image from "next/image";
 import React from "react";
-import PerfectScrollbar from "react-perfect-scrollbar";
-import { useRouter } from "next/navigation";
 import Header from "./components/Header";
 import About from "./components/About";
 import Projects from "./components/Projects";
 
 const Page = () => {
-  const router = useRouter();
-  const qual = ["Ese", "A Frontend <br/> Engineer"];
-  const handleScroll = () => {
-    console.log("scrolled");
-    router.push("/about");
-  };
   return (
     <>
       <Header />
