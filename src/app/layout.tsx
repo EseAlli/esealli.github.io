fadbf61ae0e3b2d1a064205f203bfeba227de64c
EseAlli <essealli.ea@gmail.com>
2023-07-11 16:02:46 +0100

    clean up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Eseoghene Alli",
