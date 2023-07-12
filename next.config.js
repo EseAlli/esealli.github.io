62bb256a9cee2fd708dcbe92d5afa5395f9908f3
EseAlli <essealli.ea@gmail.com>
2023-07-12 06:50:25 +0100

    changed basepath

--- next.config.js
+++ next.config.js
@@ -16,8 +16,8 @@ const nextConfig = {
    *
    * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
    */
-  basePath: "/nextjs-page",
-  assetPrefix: "/nextjs-page",
+  basePath: "/",
+  assetPrefix: "/",
 
   /**
    * Disable server-based image optimization. Next.js does not support
