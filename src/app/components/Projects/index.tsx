8c6ef3fa07863d14ae79a984e808e0660d7baef5
EseAlli <essealli.ea@gmail.com>
2023-07-12 08:08:31 +0100

    added playground projects

--- src/app/components/Projects/index.tsx
+++ src/app/components/Projects/index.tsx
@@ -1,10 +1,21 @@
-import { projects } from "@/app/utils";
+import { projects, playground } from "@/app/utils";
 import React from "react";
 import { TbBrandTypescript } from "react-icons/tb";
 import { AiOutlineQuestion } from "react-icons/ai";
 import { IoLogoReact, IoLogoNodejs } from "react-icons/io5";
-import { SiNextdotjs, SiReactquery, SiHasura } from "react-icons/si";
-import { BiLogoGraphql, BiLogoTailwindCss } from "react-icons/bi";
+import { MdOutlineHtml, MdOutlineJavascript } from "react-icons/md";
+import {
+  SiNextdotjs,
+  SiReactquery,
+  SiHasura,
+  SiAxios,
+  SiCss3,
+} from "react-icons/si";
+import {
+  BiArrowToRight,
+  BiLogoGraphql,
+  BiLogoTailwindCss,
+} from "react-icons/bi";
 import Image from "next/image";
 import { StaticImport } from "next/dist/shared/lib/get-img-props";
 
@@ -27,6 +38,14 @@ const Projects = () => {
         return <SiReactquery />;
       case "hasura":
         return <SiHasura />;
+      case "axios":
+        return <SiAxios />;
+      case "css":
+        return <SiCss3 />;
+      case "html":
+        return <MdOutlineHtml />;
+      case "javascript":
+        return <MdOutlineJavascript />;
       default:
         return <AiOutlineQuestion />;
     }
@@ -37,12 +56,14 @@ const Projects = () => {
     tags,
     url,
     img,
+    stack,
   }: {
     className?: string;
     name: string;
     tags: string[];
     url: string;
     img?: StaticImport | string;
+    stack: string;
   }) => {
     return (
       <div
@@ -60,7 +81,7 @@ const Projects = () => {
             return <div key={index}>{getTagIcon(tag)}</div>;
           })}
         </div>
-        <p className="text-sm mt-2">FRONTEND, BACKEND</p>
+        <p className="text-sm mt-2">{stack}</p>
         {img && (
           <div className="absolute top-0 right-0 hidden group-hover:block mt-5">
             <Image src={img} alt={name} width={50} height={50} />
@@ -77,21 +98,48 @@ const Projects = () => {
       <div className="md:ml-[100px] py-8 md:py-16 lg:py-24 w-full">
         <div className="container mx-auto px-4">
           <p className="mb-5 lg:mb-12 uppercase text-lime">Projects</p>
-          <div className="grid md:grid-cols-2 w-full md:gap-x-5">
-            {projects.map(({ name, tags, url, img }) => {
-              return (
-                <>
-                  <ProjectItem
-                    className="border-t border-t-white"
-                    name={name}
-                    tags={tags}
-                    url={url}
-                    img={img}
-                  />
-                </>
-              );
-            })}
+          <div className="grid md:grid-cols-2 gap-y-5 md:gap-y-0 w-full md:gap-x-5 items-center">
+            <div>
+              <p className="uppercase mb-2 font-semibold">Work</p>
+              {projects.map(({ name, tags, url, img, stack }) => {
+                return (
+                  <>
+                    <ProjectItem
+                      className="border-t border-t-white"
+                      name={name}
+                      tags={tags}
+                      url={url}
+                      img={img}
+                      stack={stack}
+                    />
+                  </>
+                );
+              })}
+            </div>
+
+            <div>
+              <p className="uppercase mb-2 font-semibold">Playground</p>
+              {playground.map(({ name, tags, url, stack }) => {
+                return (
+                  <>
+                    <ProjectItem
+                      className="border-t border-t-white"
+                      name={name}
+                      tags={tags}
+                      url={url}
+                      stack={stack}
+                    />
+                  </>
+                );
+              })}
+            </div>
           </div>
+          <a
+            className="text-lime flex gap-2 items-center mt-2"
+            href="/frontend-playground/"
+          >
+            View More <BiArrowToRight />
+          </a>
         </div>
       </div>
     </section>
--- src/app/utils/index.ts
+++ src/app/utils/index.ts
@@ -4,34 +4,73 @@ export const projects = [
     tags: ["reactjs", "tailwind", "react-query"],
     url: "https://summit.shecodeafrica.org/",
     img: "/sca.png",
+    stack: "Frontend",
   },
   {
     name: "PicknFix",
     tags: ["typescript", "nextjs", "graphql", "hasura"],
     url: "https://app.picknfix.com/welcome",
     img: "/picknfix.svg",
+    stack: "Frontend",
   },
   {
     name: "Supplybase",
     tags: ["typescript", "reactjs", "node"],
     url: "http://www.supplybase.ng/",
     img: "/supplybase.png",
+    stack: "Frontend, Backend",
   },
   {
     name: "Dalatek",
     tags: ["typescript", "nextjs", "graphql"],
     url: "https://dalatek.net/en",
     img: "/dalatek.png",
+    stack: "Frontend",
   },
   {
     name: "She Code Africa Website",
     tags: ["reactjs", "tailwind", "react-query"],
     url: "https://shecodeafrica.org/",
     img: "/sca.png",
+    stack: "Frontend",
+  },
+];
+
+export const playground = [
+  {
+    name: "Hangman",
+    tags: ["reactjs", "axios", "css"],
+    url: "https://hangman-puce.vercel.app/",
+    stack: "Frontend",
   },
   {
-    name: "Shop-X",
-    tags: ["typescript", "nextjs", "tailwind"],
-    url: "https://shop-x-rho.vercel.app/",
+    name: "Club Banana",
+    tags: ["nextjs", "tailwind"],
+    url: "https://next-club-banana-ui.vercel.app/",
+    stack: "Frontend",
+  },
+  {
+    name: "Tip Calculator",
+    tags: ["html", "css", "javascript"],
+    url: "https://tip-calculator-esealli.vercel.app/",
+    stack: "Frontend",
+  },
+  {
+    name: "Art Gallery",
+    tags: ["html", "css"],
+    url: "https://art-gallery-esealli.vercel.app/index.html",
+    stack: "Frontend",
+  },
+  {
+    name: "Sunnyside Agency",
+    tags: ["html", "css", "javascript"],
+    url: "/sunnyside-agency/",
+    stack: "Frontend",
+  },
+  {
+    name: "Loop Studios",
+    tags: ["html", "css", "javascript"],
+    url: "/loopstudio/",
+    stack: "Frontend",
   },
 ];
